cce9b77cb6d19a2f03f08c944beb5ac8bb864a0a
MoneyMonke17 <ishanpunnaivanam@gmail.com>
2025-03-03 20:12:54 -0500

    Update script.js

--- public/script.js
+++ public/script.js
@@ -159,6 +159,8 @@ function previousMonth() {
 }
 
 function nextMonth() {
+      console.log("This message will appear in the console.");
+
     alert("inside next calendar");
     fetch('/api/getevents', {
         method: 'POST',
